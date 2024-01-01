c930e43a4e61ac30d9085dd588223ed474264219
yahm0003 <110844494+yahm0003@users.noreply.github.com>
2024-01-01 19:56:51 +1100

    initial commit
    base app complete

---
+++ script.js
@@ -0,0 +1,58 @@
+const apiKey = '890b33a5e02928ff91848565859e603f';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric'
+
+const searchBox = document.querySelector(".search input")
+const searchButton = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(city) {
+    // fetch weather details through API
+    const response = await fetch(apiUrl + `&q=${city}` + "&appid=" + apiKey);
+    var data = await response.json();
+    
+    // invalid search
+    if (response.status == 404) {
+        document.querySelector('.error').style.display = 'block'
+        document.querySelector(".weather").style.display = "none"
+
+    } 
+
+    else {
+    // change weather details
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temperature').innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = data.wind.speed + "km/h";
+    
+    // change weather icon
+    if (data.weather[0].main == 'Clear') {
+        weatherIcon.src = "images/clear.png"
+    }
+    else if (data.weather[0].main == 'Clouds') {
+        weatherIcon.src = "images/clouds.png"
+    }
+    else if (data.weather[0].main == 'Rain') {
+        weatherIcon.src = "images/rain.png"
+    }
+    else if (data.weather[0].main == 'Drizzle') {
+        weatherIcon.src = "images/drizzle.png"
+    }
+    else if (data.weather[0].main == 'Mist') {
+        weatherIcon.src = "images/mist.png"
+    }
+
+    document.querySelector(".weather").style.display = "block"
+    document.querySelector(".error").style.display = "none"
+
+}}
+
+// click to submit
+searchButton.addEventListener('click', () => {
+    checkWeather(searchBox.value);
+})
+// enter key submit
+searchBox.addEventListener('keypress', function (event) {
+    if (event.key == "Enter") {
+        searchButton.click()
+    }
+})
